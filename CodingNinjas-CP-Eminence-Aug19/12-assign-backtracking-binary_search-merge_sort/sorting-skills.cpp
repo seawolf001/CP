3d8e94408dcf9cf4aee1c41ffea1ec99273458f0
Jitendra Kumar <jitendra.iitp@gmail.com>
2019-09-07 19:14:32 +0530

    Chapter 12: Backtracking-MergeSort-BinarySearch assignments

---
+++ CodingNinjas-CP-Eminence-Aug19/12-assign-backtracking-binary_search-merge_sort/sorting-skills.cpp
@@ -0,0 +1,36 @@
+/*
+ * Author: Jitendra Kumar (seawolf001)
+ * Email: jitendra.iitp@gmail.com
+ */
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define INF LLONG_MAX/4
+#define MOD 1e9+7
+#define DEBUG(x) cerr << #x << " is " << (x) << endl;
+
+typedef long long ll;
+typedef long double ld;
+typedef std::vector<ll> vll;
+typedef std::vector<int> vi;
+typedef std::vector<string> vs;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    /* Start writing from here */
+    int t=0; cin>>t;
+    while(t--) {
+        int n=0; cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++) { cin>>arr[i]; }
+        for(int i=1;i<n;i++) {
+
+        }
+    }
+    return 0;
+}
+
+
