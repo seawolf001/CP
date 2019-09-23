f5c7950f731567aa20ed5b233d31edbc53105266
Jitendra Kumar <jitendra.iitp@gmail.com>
2019-09-23 23:53:15 +0530

    Finished Graph2

--- CodingNinjas-CP-Eminence-Aug19/21-Graph2/dijkstras.cpp
+++ CodingNinjas-CP-Eminence-Aug19/21-Graph2/dijkstras.cpp
@@ -11,13 +11,14 @@ typedef std::vector<ll> vll;
 typedef std::vector<int> vi;
 typedef std::vector<string> vs;
 
-void dijkstras(vector<pair<int, int>> adj[], int n) {
+typedef pair<int, int> iPair;
+
+void dijkstras(vector<iPair> adj[], int n) {
     int * d = new int[n];
-    for(int i=0;i<n;i++) { d[i]=INT_MAX; }
-    d[0]=0;
     bool * vis = new bool[n];
-    memset (vis, false, n*sizeof (bool));
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    for(int i=0;i<n;i++) { d[i]=INT_MAX; vis[i]=false;}
+    d[0]=0;
+    priority_queue<iPair, vector <iPair> , greater<iPair> > pq;
     pq.push(make_pair(0,0));
     while(!pq.empty()) {
         int u = pq.top().second;
@@ -26,7 +27,7 @@ void dijkstras(vector<pair<int, int>> adj[], int n) {
         for(auto it=adj[u].begin(); it!=adj[u].end(); it++) {
             int v = (*it).first;
             int w = (*it).second;
-            if(!vis[v] && (d[v] > w + d[u])) {
+            if(vis[v] == false && d[u]!=INT_MAX && (d[v] > w + d[u])) {
                 d[v] = w + d[u];
                 pq.push(make_pair(d[v], v));
             }
@@ -41,14 +42,17 @@ int main() {
     /* Start writing from here */
     int n, m, x, y, w;
     cin >> n >> m;
-    vector<pair<int, int>> g[n];
+    vector<iPair> g[n];
     for(int i=0;i<m;i++) {
         cin >> x >> y >> w;
-        g[x].push_back(make_pair(y,w));
-        g[y].push_back(make_pair(x,w));
+        if (w!=0) {
+            g[x].push_back(make_pair(y,w));
+            g[y].push_back(make_pair(x,w));
+        }
     }
     dijkstras(g, n);
     return 0;
 }
+
 
 
