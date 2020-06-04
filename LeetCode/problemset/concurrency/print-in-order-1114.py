2a3ae519904a9a6572cbfa1ceca920da6393fbce
Jitendra Kumar <jitendra.iitp@gmail.com>
2020-06-04 23:47:23 +0530

    Leetcode:Problemset:concurrency:[WIP]

---
+++ LeetCode/problemset/concurrency/print-in-order-1114.py
@@ -0,0 +1,24 @@
+from threading import Semaphore
+
+class Foo:
+    def __init__(self):
+        self.semaphores = (Semaphore(0), Semaphore(0))
+
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+        # printFirst() outputs "first". Do not change or remove this line.
+        printFirst()
+        self.semaphores[0].release()
+
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        # printSecond() outputs "second". Do not change or remove this line.
+        with self.semaphores[0]:
+            printSecond()
+            self.semaphores[1].release()
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        # printThird() outputs "third". Do not change or remove this line.
+        with self.semaphores[1]:
+            printThird()
+
