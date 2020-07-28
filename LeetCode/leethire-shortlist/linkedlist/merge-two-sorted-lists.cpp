f15bda037f92411df5448b901485619d2002189a
Jitendra Kumar <jitendra.iitp@gmail.com>
2020-07-28 16:50:32 +0530

    Started solving leethire-shortlists:linkedlists

---
+++ LeetCode/leethire-shortlist/linkedlist/merge-two-sorted-lists.cpp
@@ -0,0 +1,40 @@
+/* Author: Jitendra Kumar (jitendra.iitp@gmail.com) */
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define INF LLONG_MAX/4
+#define MOD 1e9+7
+#define DEBUG(x) cerr << #x << " is " << (x) << endl;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode *head = new ListNode(), *curr=head;
+        while (l1 && l2) {
+            int x = l1->val;
+            int y = l2->val;
+            if(x <= y) {
+                curr->next = l1;
+                l1 = l1->next;
+            } else {
+                curr->next = l2;
+                l2 = l2->next;
+            }
+            curr = curr->next;
+        }
+        if(l1) { curr->next = l1; }
+        else if(l2) { curr->next = l2; }
+        return head->next;
+    }
+};
+
---
+++ LeetCode/leethire-shortlist/linkedlist/remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,36 @@
+
+/* Author: Jitendra Kumar (jitendra.iitp@gmail.com) */
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define INF LLONG_MAX/4
+#define MOD 1e9+7
+#define DEBUG(x) cerr << #x << " is " << (x) << endl;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if (!head) return head;
+        else if(n==0) return head;
+        ListNode **slow=&head, *fast=head;
+        while (n--) { fast = fast->next; }
+        while (fast) {
+            fast = fast->next;
+            slow = &((*slow)->next);
+        }
+        *slow = (*slow)->next;
+        return head;
+    }
+};
+
