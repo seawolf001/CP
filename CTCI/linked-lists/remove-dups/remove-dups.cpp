2702ac2283d8072bc3eec44f5b7c164ad07099ab
Jitendra Kumar <jitendra.iitp@gmail.com>
2020-09-12 23:47:56 +0530

    [WIP]more problems from CTCI

---
+++ CTCI/linked-lists/remove-dups/remove-dups.cpp
@@ -0,0 +1,25 @@
+/* Author: Jitendra Kumar (jitendra.iitp@gmail.com) */
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define INF LLONG_MAX/4
+#define MOD 1e9+7
+#define DEBUG(x) cerr << #x << " is " << (x) << endl;
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        // TODO
+    }
+};
