a6365d94efac03adc6fdc6a9a6132259bf5d295f
Jitendra Kumar <jitendra.iitp@gmail.com>
2021-01-09 20:01:55 +0530

    [SOLVED] Leetcode::minimum-ascii-delete-sum-for-two-strings

---
+++ LeetCode/problemset/algorithms/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -0,0 +1,34 @@
+/* Author: Jitendra Kumar (jitendra.iitp@gmail.com) */
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define INF LLONG_MAX/4
+#define MOD 1e9+7
+#define DEBUG(x) cerr << #x << " is " << (x) << endl;
+class Solution {
+public:
+    int minimumDeleteSum(string a, string b) {
+        // Need to delete all the chars that are not in LCS. ==> min possible.
+        int n = a.size();
+        int m = b.size();
+        vector<vector<int>> dp(n+1 , vector<int> (m+1));
+
+        for(int i=1 ; i<=n ; i++)                 // base case filling up
+            dp[i][0] = dp[i-1][0] + a[i-1];
+
+        for(int j=1; j<=m ; j++)                 // base case filling up
+            dp[0][j] = dp[0][j-1] + b[j-1];
+
+        for(int i=1 ; i<=n ; i++) {
+            for(int j=1 ; j<=m ; j++) {
+                if(a[i-1] == b[j-1])
+                    dp[i][j] = dp[i-1][j-1];
+                else
+                    dp[i][j] = min( dp[i-1][j] + a[i-1] ,
+                                    dp[i][j-1] + b[j-1] );
+            }
+        }
+        return dp[n][m];
+    }
+};
+
