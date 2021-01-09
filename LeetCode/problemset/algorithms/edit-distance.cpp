28bbde4f21b81c24f4ef0dcb50725717eda25696
Jitendra Kumar <jitendra.iitp@gmail.com>
2021-01-09 19:25:47 +0530

    [SOLVED] Leetcode::edit-distance

---
+++ LeetCode/problemset/algorithms/edit-distance.cpp
@@ -0,0 +1,31 @@
+/* Author: Jitendra Kumar (jitendra.iitp@gmail.com) */
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define INF LLONG_MAX/4
+#define MOD 1e9+7
+#define DEBUG(x) cerr << #x << " is " << (x) << endl;
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        int dp[n+1][m+1];
+        for(int i=0;i<=n;i++) {
+            for(int j=0;j<=m;j++) {
+                if(i==0) {
+                    dp[i][j]=j;
+                } else if(j==0){
+                    dp[i][j]=i;
+                } else if(word1[i-1]==word2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = 1 + min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1]));
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
+
