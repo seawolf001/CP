1814eb3d3055efea259e3a72a817d3be95af3ee7
Jitendra Kumar <jitendra.iitp@gmail.com>
2021-01-16 02:40:52 +0530

    Leetcode::factorial-trailing-zeroes

---
+++ LeetCode/tags/math/factorial-trailing-zeroes.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define MOD 1000000007
+typedef vector<int> vi;
+typedef vector<vector<int>> vii;
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int result = 0;
+        for(long long i=5; n/i>0; i*=5){
+            result += (n/i);
+        }
+        return result;
+    }
+};
