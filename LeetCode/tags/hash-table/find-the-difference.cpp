6a7ab3fef15502f9342b9440eb4442c58961c191
Jitendra Kumar <jitendra.iitp@gmail.com>
2021-01-17 03:45:47 +0530

    Leetcode::find-the-difference

---
+++ LeetCode/tags/hash-table/find-the-difference.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define MOD 1000000007
+typedef vector<int> vi;
+typedef vector<vector<int>> vii;
+
+//https://leetcode.com/problems/find-the-difference/discuss/86826/Concise-C%2B%2B-solution-using-XOR
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char r=0;
+        for(char c:s) r ^=c;
+        for(char c:t) r ^=c;
+        return r;
+    }
+};
