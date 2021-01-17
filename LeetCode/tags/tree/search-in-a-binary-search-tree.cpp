46e3da0cec27f8c621e55c50b8494112faef8584
Jitendra Kumar <jitendra.iitp@gmail.com>
2021-01-17 22:57:23 +0530

    Leetcode::search-in-a-binary-search-tree

---
+++ LeetCode/tags/tree/search-in-a-binary-search-tree.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define MOD 1000000007
+typedef vector<int> vi;
+typedef vector<vector<int>> vii;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* node, int val) {
+        if(node==NULL) return node;
+        else if(node->val == val) return node;
+        else if(val < node->val) return searchBST(node->left, val);
+        else return searchBST(node->right, val);
+    }
+};
