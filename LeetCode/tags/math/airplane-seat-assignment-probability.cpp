9e6c5d3254b8416054b789f1daf4c98bfc7b66e6
Jitendra Kumar <jitendra.iitp@gmail.com>
2021-01-20 12:31:11 +0530

    Leetcode::airplane-seat-assignment-probability

---
+++ LeetCode/tags/math/airplane-seat-assignment-probability.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define MOD 1000000007
+typedef vector<int> vi;
+typedef vector<vector<int>> vii;
+
+class Solution {
+public:
+    double nthPersonGetsNthSeat(int n) {
+        return n==1?1:0.5;
+    }
+};
