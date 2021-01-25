ae0b3235a6f42e673b4c4cc17ace2db307c6d8ef
Jitendra Kumar <jitendra.iitp@gmail.com>
2021-01-25 20:59:16 +0530

    Added "sliding-window" tag in leetcode/tags

---
+++ LeetCode/tags/sliding-window/subarrays-with-k-different-integers.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+// #define endl '\n'
+// #define MOD 1000000007
+
+bool az(int x) { return 97<=x && x<=122; }
+bool AZ(int x) { return 65<=x && x<=90; }
+bool number(int x) { return 48<=x && x<=57; }
+
+
