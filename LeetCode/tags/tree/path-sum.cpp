5c842ea3d6686bd7d54fcd0433dc48f332a37295
Jitendra Kumar <jitendra.iitp@gmail.com>
2021-02-09 06:37:34 +0530

    path-sum

---
+++ LeetCode/tags/tree/path-sum.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+// #define endl '\n'
+// #define MOD 1000000007
+
+bool az(int x) { return 97<=x && x<=122; }
+bool AZ(int x) { return 65<=x && x<=90; }
+bool number(int x) { return 48<=x && x<=57; }
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* node, int targetSum) {
+        if(!node)
+            return false;
+        if(!node->left && !node->right)
+            return targetSum - node->val == 0;
+        return hasPathSum(node->left, targetSum - node->val) ||
+                hasPathSum(node->right, targetSum - node->val);
+    }
+};
+
