243f68b78bf6c6bbc1ce19ee7c3c3ed3098ea14f
Jitendra Kumar <jitendra.iitp@gmail.com>
2021-03-28 21:04:07 +0530

    remove-duplicates-from-sorted-list

---
+++ LeetCode/tags/linkedlist/remove-duplicates-from-sorted-list.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+// #define endl '\n'
+// #define MOD 1000000007
+
+bool az(int x) { return 97<=x && x<=122; }
+bool AZ(int x) { return 65<=x && x<=90; }
+bool number(int x) { return 48<=x && x<=57; }
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(!head || !head->next)
+            return head;
+        ListNode *next = head->next, *curr=head;
+        while(next) {
+            while(next && curr->val == next->val)
+                next = next->next;
+            curr->next = next;
+            curr = next;
+            next = next->next;
+        }
+        return head;
+    }
+};
