68db4c0dd3a3597cdc98cf93afb37764debcc51b
Jitendra Kumar <jitendra.iitp@gmail.com>
2021-04-05 17:07:44 +0530

    [Optimized] Leetcode::longest-consecutive-sequence

--- LeetCode/tags/array/longest-consecutive-sequence.cpp
+++ LeetCode/tags/array/longest-consecutive-sequence.cpp
@@ -1,23 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-// #define endl '\n'
-// #define MOD 1000000007
 
-bool az(int x) { return 97<=x && x<=122; }
-bool AZ(int x) { return 65<=x && x<=90; }
-bool number(int x) { return 48<=x && x<=57; }
+// Time complexity: O(n), Space: O(n)
 
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        int n = nums.size(),  ans=1, curr=0;
-        if(n==0) return 0;
+        if(nums.size()==0)
+            return 0;
         unordered_set<int> s;
         for(int x: nums)  s.insert(x);
-        for(int x: nums) {
+        int ans = 1;
+        for(int x: s) {
+            if(s.find(x-1) != s.end()) continue;
             int y = x+1;
-            while(s.find(x-1) == s.end() && s.find(y) != s.end())
-                ++y;
+            while(s.find(y) != s.end())  ++y;
             ans = max(ans, y-x);
         }
         return ans;
