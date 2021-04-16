a36e81561ca0a2180fabd4ba5b2eb5bf57ab6ea2
Jitendra Kumar <jitendra.iitp@gmail.com>
2021-04-16 15:52:29 +0530

    permutation-sequence.cpp

---
+++ LeetCode/tags/math/permutation-sequence.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+// #define endl '\n'
+// #define MOD 1000000007
+
+bool az(int x) { return 97<=x && x<=122; }
+bool AZ(int x) { return 65<=x && x<=90; }
+bool number(int x) { return 48<=x && x<=57; }
+
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        int fact = 1;
+        string s = "";
+        for(int i=1;i<n;i++) {
+            fact *= i;
+            s += to_string(i);
+        }
+        s += to_string(n);
+        string ans = "";
+        --k; // 0-based indexing
+        while(true) {
+            int idx = k / fact;
+            ans += s[idx];
+            s.erase(idx, 1);
+            if(s.size() == 0)
+                break;
+            k = k % fact;
+            fact = fact / s.size();
+            cout << idx << " " << k << " " << fact << endl;
+        }
+        return ans;
+    }
+};
