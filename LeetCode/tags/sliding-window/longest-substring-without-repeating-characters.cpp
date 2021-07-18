05109f91eca9a7d3f0d72ef97c10ca073f9f9372
Jitendra Kumar <jitendra.iitp@gmail.com>
2021-07-18 18:56:36 +0530

    longest-substring-without-repeating-characters.cpp

--- LeetCode/tags/sliding-window/longest-substring-without-repeating-characters.cpp
+++ LeetCode/tags/sliding-window/longest-substring-without-repeating-characters.cpp
@@ -11,15 +11,15 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string str) {
         int maxL=0, n=str.length(), s=0;
-        unordered_map<char, int> _map;
-        for (int i=0;i<n;i++) {
-            char c=str[i];
-            if (_map.count(c)!=0) {
-                s = max(s, _map[c]+1);
-            }
-            _map[c]=i;
+        unordered_map<char, int> occur;
+        for(int i=0;i<n;i++) {
+            char c = str[i];
+            if(occur.find(c) != occur.end())
+                s = max(s, occur[c]+1);
             maxL = max(maxL, i-s+1);
+            occur[c] = i;
         }
         return maxL;
     }
 };
+
