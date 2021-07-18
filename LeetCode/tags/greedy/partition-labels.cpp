7e2a3975e58034b6df884b3625def0f54bbb6b57
Jitendra Kumar <jitendra.iitp@gmail.com>
2021-07-18 19:39:44 +0530

    partition-labels.cpp

--- LeetCode/tags/greedy/partition-labels.cpp
+++ LeetCode/tags/greedy/partition-labels.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define endl '\n'
-#define MOD 1000000007
+// #define endl '\n'
+// #define MOD 1000000007
 
 bool az(int x) { return 97<=x && x<=122; }
 bool AZ(int x) { return 65<=x && x<=90; }
@@ -10,17 +10,20 @@ bool number(int x) { return 48<=x && x<=57; }
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        unordered_map<char, int> mp;
-        for(int i=0;i<s.size(); i++) mp[s[i]]=i;
+        unordered_map<char, int> occur;
+        int n = s.size();
+        for(int i=0;i<n;i++)
+            occur[s[i]] = i;
         vector<int> ans;
-        for(int i=0;i<s.size();) {
-            int last = mp[s[i]], prev=i;
-            // choose i->last but if in b/w others chars then take till mp[c]
-            while(i<=last) last = max(last, mp[s[i]]), ++i;
-            ans.push_back(i-prev);
+        for(int i=0;i<n;) {
+            int last = occur[s[i]], start=i; // Need to consider till last occurance of this char
+            while(i<=last) {
+                last = max(last, occur[s[i]]); // consider till last of other chars if came in between.
+                ++i;
+            }
+            ans.push_back(i-start); // append the distance.
         }
         return ans;
     }
 };
-
 
